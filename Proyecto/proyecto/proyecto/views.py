be2a3fab53bf7b00acb6571b8b108005633b1643
IsaLanCe <landacervantesisaias@gmail.com>
2025-05-09 16:29:06 -0600

    Token y ChatID cifrados en el archivo cifrado de credenciales

--- Proyecto/proyecto/proyecto/views.py
+++ Proyecto/proyecto/proyecto/views.py
@@ -8,7 +8,54 @@ import random
 import string
 import sys
 import requests
+import re
+import os
 
+to = os.environ.get('TOKEN_T')
+chat = os.environ.get('CHAT_ID')
+
+def campo_vacio(campo):
+    return campo.strip() == ''
+
+def validar_campo(campo):
+    if re.match(r'^[a-zA-Z0-9 _-]+$', campo):
+        return False
+    else:
+        return True
+	
+def generar_otp():
+	size = 6
+	otp = ''.join([random.choice( string.ascii_uppercase + string.ascii_lowercase + string.digits ) for n in range(size)])
+
+	return otp
+
+def eliminar_otps_anteriores():
+	OTP.objects.all().delete()
+
+def guardar_otp(code):
+	eliminar_otps_anteriores()
+	return OTP.objects.create(code=code)
+
+def enviar_otp_telegram(code):
+	
+	token = to
+	chat_id = chat
+	url = f'https://api.telegram.org/bot{token}/sendMessage'
+	mensaje = f"Tu token de validación es: {code}"
+    
+	datos = {
+		'chat_id':chat_id,
+		'text':mensaje,
+		'parse_mode':'Markdown'
+	}
+
+	try:
+		response = requests.post(url, data=datos)
+		response.raise_for_status()
+		return True
+	except requests.RequestException as e:
+		print(f'Error: {e}')
+		return False
 
 def login(request):
 	t = "login.html"
@@ -38,7 +85,6 @@ def login(request):
 			errores.append("Lo sentimos, hubo una falla")
 			return render(request, 'login.html', {'errores':errores})
 		
-
 def panel(request):
 	p = 'panel.html'
 	if request.method == 'GET':
@@ -54,39 +100,6 @@ def administrar_servicios(request):
 	if request.method == 'GET':
 		return render(request,a)
 
-def generar_otp():
-	size = 6
-	otp = ''.join([random.choice( string.ascii_uppercase + string.ascii_lowercase + string.digits ) for n in range(size)])
-
-	return otp
-
-def eliminar_otps_anteriores():
-	OTP.objects.all().delete()
-
-def guardar_otp(code):
-	eliminar_otps_anteriores()
-	return OTP.objects.create(code=code)
-
-def enviar_otp_telegram(code):
-	token = '8005722361:AAEPjTpW9IzQYH_TSzwr7-UfgE4G0gdE0NA'
-	chat_id = '5058444764'
-	url = f'https://api.telegram.org/bot{token}/sendMessage'
-	mensaje = f"Tu token de validación es: {code}"
-    
-	datos = {
-		'chat_id':chat_id,
-		'text':mensaje,
-		'parse_mode':'Markdown'
-	}
-
-	try:
-		response = requests.post(url, data=datos)
-		response.raise_for_status()
-		return True
-	except requests.RequestException as e:
-		print(f'Error: {e}')
-		return False
-
 def generar_otp_view(request):
 	v = 'verificar_otp.html'
 	if request.method == 'POST':
@@ -99,11 +112,15 @@ def generar_otp_view(request):
 
 def verificar_otp_view(request):
 	errores = []
+	v = 'verificar_otp.html'
+	a = 'administrar_servicios.html'
 
 	if request.method == 'POST':
 		otp_input = request.POST.get('otp')
-		if not otp_input:
+		if campo_vacio(otp_input):
 			errores.append("El codigo de verificación es obligatorio")
+		elif validar_campo(otp_input):
+			errores.append("No debe tener caracteres especiales")
 		else:
 			try:
 				otp = OTP.objects.get(code=otp_input, esta_usado=False)
